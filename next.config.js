079fe648dcb59abf5cbfc42d4be0203e5844c0d6
EL-SHERIEF <entkal@outlook.com>
2025-10-22 09:25:10 +0300

    nex-app-roter

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-    ANTHROPIC_API_KEY: process.env.ANTHROPIC_API_KEY,
-  },
-  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  experimental: {
+    appDir: true,
+  },
 };
 
 module.exports = nextConfig;
